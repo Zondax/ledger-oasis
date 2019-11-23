f5cbd661e5f0b20410cdacc15b3302efaa1c646f
Juan Leni <lenijuan@gmail.com>
2019-11-23 21:43:46 +0100

    basic initial tests

--- tests/ui_output.cpp
+++ tests/ui_output.cpp
@@ -20,5 +20,137 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include "lib/parser.h"
+#include "util/base64.h"
+#include "util/common.h"
 
-// TODO: Write UI test cases
+using ::testing::TestWithParam;
+using ::testing::Values;
+using json = nlohmann::json;
+
+typedef struct {
+//    std::string nonce;
+//    std::string fee_amount;
+//    std::string fee_gas;
+//    std::string method;
+//    std::string body;
+} tx_t;
+
+typedef struct {
+    std::string index;
+    std::string kind;
+    std::string signature_context;
+    std::string encoded_tx;
+    bool valid;
+
+//    std::string parsingErr;
+////    std::string signed_tx;
+//    std::string encoded_signed_tx;
+//    bool valid;
+//    std::string signer_private_key;
+//    std::string signer_public_key;
+} testcase_t;
+
+class JsonTests : public ::testing::TestWithParam<testcase_t> {
+public:
+    struct PrintToStringParamName {
+        template<class ParamType>
+        std::string operator()(const testing::TestParamInfo<ParamType> &info) const {
+            auto p = static_cast<testcase_t>(info.param);
+            std::stringstream ss;
+            ss << p.index << "_" << p.kind;
+            return ss.str();
+        }
+    };
+};
+
+std::vector<testcase_t> GetJsonTestCases() {
+    auto answer = std::vector<testcase_t>();
+
+    json j;
+    std::ifstream inFile("testcases.json");
+    EXPECT_TRUE(inFile.is_open())
+                        << "\n"
+                        << "******************\n"
+                        << "Check that your working directory points to the tests directory\n"
+                        << "In CLion use $PROJECT_DIR$\\tests\n"
+                        << "******************\n";
+    if (!inFile.is_open())
+        return answer;
+
+    // Retrieve all test cases
+    inFile >> j;
+    std::cout << "Number of testcases: " << j.size() << std::endl;
+
+    int count = 0;
+    for (auto &item : j) {
+        count++;
+        answer.push_back(testcase_t{
+                std::to_string(count),
+                item["kind"],
+                item["signature_context"],
+                item["encoded_tx"],
+                item["valid"]
+        });
+    }
+
+    return answer;
+}
+
+//void validate_testcase(const testcase_t &tc) {
+//    parser_context_t ctx;
+//    parser_error_t err;
+//
+//    const auto *buffer = (const uint8_t *) tc.tx.c_str();
+//    uint16_t bufferLen = tc.tx.size();
+//
+//    err = parser_parse(&ctx, buffer, bufferLen);
+//    ASSERT_EQ(parser_getErrorDescription(err), tc.parsingErr) << "Parsing error mismatch";
+//
+//    if (err != parser_ok)
+//        return;
+//
+//    err = parser_validate(&ctx);
+//    EXPECT_EQ( parser_getErrorDescription(err), tc.validationErr) << "Validation error mismatch";
+//}
+
+//TEST_P(JsonTests, ValidateTestcase) { validate_testcase(GetParam()); }
+
+void check_testcase(const testcase_t &tc) {
+    parser_context_t ctx;
+    parser_error_t err;
+
+    std::string cborString;
+    macaron::Base64::Decode(tc.encoded_tx, cborString);
+
+    const auto *buffer = (const uint8_t *) cborString.c_str();
+    uint16_t bufferLen = cborString.size();
+
+    err = parser_parse(&ctx, buffer, bufferLen);
+    if (tc.valid) {
+        ASSERT_EQ(err, parser_ok);
+    }
+
+    auto output = dumpUI(&ctx, 40, 40);
+
+    for (const auto &i : output) {
+        std::cout << i << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+
+//    EXPECT_EQ(output.size(), tc.expected.size());
+//    for (size_t i = 0; i < tc.expected.size(); i++) {
+//        if (i < output.size()) {
+//            EXPECT_THAT(output[i], testing::Eq(tc.expected[i]));
+//        }
+//    }
+}
+
+INSTANTIATE_TEST_CASE_P
+
+(
+        JsonTestCases,
+        JsonTests,
+        ::testing::ValuesIn(GetJsonTestCases()), JsonTests::PrintToStringParamName()
+);
+
+TEST_P(JsonTests, CheckUIOutput) { check_testcase(GetParam()); }
