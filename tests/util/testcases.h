8f9fa8427af4b42556c42bc5f96347fdaf346e48
Juan Leni <lenijuan@gmail.com>
2019-12-13 14:59:48 +0100

    Refactoring + speeding up test cases

---
+++ tests/util/testcases.h
@@ -0,0 +1,44 @@
+/*******************************************************************************
+*   (c) 2019 ZondaX GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+#pragma once
+#include <json/json.h>
+#include <fstream>
+
+typedef struct {
+    bool empty;
+    std::string description;
+    std::string index;
+    std::string kind;
+    std::string signature_context;
+    std::string encoded_tx;
+    bool valid;
+    std::vector<std::string> expected_ui_output;
+} testcaseData_t;
+
+typedef struct {
+    std::shared_ptr<Json::Value> testcases;
+    int64_t index;
+    std::string description;
+} testcase_t;
+
+testcaseData_t ReadTestCaseData(std::shared_ptr<Json::Value> jsonSource, int index);
+
+std::vector<testcase_t> GetJsonTestCases(const std::string& filename);
+
+std::vector<std::string> GenerateExpectedUIOutput(std::string context, Json::Value j);
+
+bool TestcaseIsValid(const Json::Value &tc);
+
