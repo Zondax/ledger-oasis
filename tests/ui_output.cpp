367c010d542614d896642e0f6bdd87ac796b97f2
Juan Leni <juan.leni@zondax.ch>
2019-12-18 22:35:29 +0100

    Improving test generation (#28)
    * partial manual test cases
    * Test for entity
    * Correct manual_testvectors.json file generated with correct version of node
    * updating dependency
    * move validation from assert to expect so errors can be seen
    * Fix manual test script (base64 and not hex); fix printng in tests;
    * fixing warning/issues in macOS
    * Enable Oasis tests
    * addressing warnings
    * updating app
    * Fixing test generation (nested CBOR still missing)
    * update template
    * improve manual test case generation
    * upgrade to v0.12.0

--- tests/ui_output.cpp
+++ tests/ui_output.cpp
@@ -19,9 +19,7 @@
 
 #include <iostream>
 #include <memory>
-#include <zxmacros.h>
 #include "lib/parser.h"
-#include "util/base64.h"
 #include "util/common.h"
 
 using ::testing::TestWithParam;
@@ -46,10 +44,10 @@ void check_testcase(const testcase_t &testcase) {
 
     err = parser_validate(&ctx);
     if (tc.valid) {
-        ASSERT_EQ(err, parser_ok) << parser_getErrorDescription(err);
+        EXPECT_EQ(err, parser_ok) << parser_getErrorDescription(err);
     } else {
         // TODO: maybe we can eventually match error codes too
-        ASSERT_NE(err, parser_ok) << parser_getErrorDescription(err);
+        EXPECT_NE(err, parser_ok) << parser_getErrorDescription(err);
         return;
     }
 
@@ -93,7 +91,7 @@ public:
     };
 };
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
         Generated,
         OasisTests,
         ::testing::ValuesIn(GetJsonTestCases("oasis_testvectors.json")), OasisTests::PrintToStringParamName()
@@ -118,7 +116,7 @@ public:
     };
 };
 
-INSTANTIATE_TEST_CASE_P(
+INSTANTIATE_TEST_SUITE_P(
         Manual,
         ManualTests,
         ::testing::ValuesIn(GetJsonTestCases("manual_testvectors.json")), ManualTests::PrintToStringParamName()
