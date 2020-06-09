626790324d9423779edae6d5a60b3b6a8fad76f8
ER <raynaudo.ee@gmail.com>
2020-06-09 11:13:29 -0300

    Fix validator sign test

--- tests_zemu/tests/val.test.js
+++ tests_zemu/tests/val.test.js
@@ -1,6 +1,6 @@
 import jest, {expect} from "jest";
 import Zemu from "@zondax/zemu";
-import Oasis from "@zondax/ledger-oasis";
+import {OasisValidatorApp} from "@zondax/ledger-oasis";
 
 const tweetnacl = require("tweetnacl");
 
@@ -40,7 +40,7 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = new Oasis.OasisValidatorApp(sim.getTransport());
+            const app = new OasisValidatorApp(sim.getTransport());
             const resp = await app.getVersion();
 
             console.log(resp);
@@ -56,7 +56,7 @@ describe('Basic checks', function () {
         }
     });
 
-    it('sign basic', async function () {
+    it('validator sign basic', async function () {
         const snapshotPrefixGolden = "snapshots/sign-basic/";
         const snapshotPrefixTmp = "snapshots-tmp/sign-basic/";
         let snapshotCount = 0;
@@ -64,22 +64,41 @@ describe('Basic checks', function () {
         const sim = new Zemu(APP_PATH);
         try {
             await sim.start(sim_options);
-            const app = new Oasis.OasisValidatorApp(sim.getTransport());
+            const app = new OasisValidatorApp(sim.getTransport());
 
             const path = [474, 474, 5, 0x80000000, 0x80000003];
             const context = "oasis-core/consensus: tx for chain testing";
 
-            const txBlob1 = Buffer.from("210801110500000000000000190000000000000000220b088092b8c398feffffff01", "hex",);
-            const txBlob2 = Buffer.from("210801110500000000000000190100000000000000220b088092b8c398feffffff01", "hex",);
-            const txBlob3 = Buffer.from("210801110500000000000000190200000000000000220b088092b8c398feffffff01", "hex",);
-            const txBlob4 = Buffer.from("210801110500000000000000190300000000000000220b088092b8c398feffffff01", "hex",);
-            const txBlob5 = Buffer.from("210801110500000000000000190400000000000000220b088092b8c398feffffff01", "hex",);
-            const txBlob6 = Buffer.from("210801110500000000000000190500000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob0 = Buffer.from("210801110500000000000000190000000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob1 = Buffer.from("210801110500000000000000190100000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob2 = Buffer.from("210801110500000000000000190200000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob3 = Buffer.from("210801110500000000000000190300000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob4 = Buffer.from("210801110500000000000000190400000000000000220b088092b8c398feffffff01", "hex",);
+            const txBlob5 = Buffer.from("210801110500000000000000190500000000000000220b088092b8c398feffffff01", "hex",);
 
-            let signatureResponse = await app.sign(path, context, txBlob1);
-            console.log(signatureResponse);
+            let signatureRequest = app.sign(path, context, txBlob0);
             await Zemu.sleep(VOTE_SLEEP);
 
+            await sim.clickRight();
+            await sim.clickRight();
+            await sim.clickRight();
+            await sim.clickRight();
+            await sim.clickBoth();
+
+            let signatureResponse = await signatureRequest;
+            console.log(signatureResponse);
+
+            expect(signatureResponse.return_code).toEqual(0x6985);
+            expect(signatureResponse.error_message).toEqual("Conditions not satisfied");
+
+            for (let i = 1; i <= 5; i++) {
+                let blob = eval("txBlob" + i)
+                signatureResponse = await app.sign(path, context, blob);
+                await Zemu.sleep(VOTE_SLEEP);
+                console.log(signatureResponse);
+                expect(signatureResponse.return_code).toEqual(0x9000);
+                expect(signatureResponse.error_message).toEqual("No errors");
+            }
 
         } finally {
             await sim.close();
