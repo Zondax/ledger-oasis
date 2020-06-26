ae8c431a4a59f64724480911be91d2741e3b5576
ER <raynaudo.ee@gmail.com>
2020-06-26 15:15:22 -0300

    Return current vote state alongside with the rejected vote

--- app/src/common/actions.c
+++ app/src/common/actions.c
@@ -43,13 +43,20 @@ void app_sign() {
         }
 
         if (!try_state_transition()) {
-            //Return the conflicting vote data
+            //Return current vote_state along with the conflicting vote data
+            // [vote_state][vote][error]
             MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
             G_io_apdu_buffer[0] = vote.Type;
             uint16_t offset = 1;
             memcpy(&G_io_apdu_buffer[offset], &vote.Height, sizeof(vote.Height));
             offset += sizeof(vote.Height);
             memcpy(&G_io_apdu_buffer[offset], &vote.Round, sizeof(vote.Round));
+            offset += sizeof(vote.Round);
+            G_io_apdu_buffer[offset] = vote_state.vote.Type;
+            offset += 1;
+            memcpy(&G_io_apdu_buffer[offset], &vote_state.vote.Height, sizeof(vote.Height));
+            offset += sizeof(vote.Height);
+            memcpy(&G_io_apdu_buffer[offset], &vote_state.vote.Round, sizeof(vote.Round));
             offset += sizeof(vote.Round);
             set_code(G_io_apdu_buffer, offset, APDU_CODE_COMMAND_NOT_ALLOWED);
             io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, offset + 2);
