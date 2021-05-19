25a1ba2429a491563c30e4cb49db9a9bd64f952f
Emmanuel <emmanuelm41@gmail.com>
2021-05-19 14:14:30 -0300

    add paging func again, it is needed to pass cpp tests

--- app/src/consumer/parser_consumer.c
+++ app/src/consumer/parser_consumer.c
@@ -959,6 +959,17 @@ parser_error_t parser_getItem(const parser_context_t *ctx,
                 return parser_unexpected_type;
         }
     }
+    ///////////////////////////////
+    ///////////////////////////////
+    // Add paging values
+    if (err == parser_ok && *pageCount > 1) {
+        size_t keyLen = strlen(outKey);
+        if (keyLen < outKeyLen) {
+            snprintf(outKey + keyLen, outKeyLen - keyLen, "[%d/%d]", pageIdx + 1, *pageCount);
+        }
+    }
+    ///////////////////////////////
+    ///////////////////////////////
 
     return err;
 }
